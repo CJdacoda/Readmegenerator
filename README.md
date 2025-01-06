46d9b0ffb8632f526b057560ff173db178a03963
CJdacoda <calebparker256@gmail.com>
2025-01-06 18:21:08 -0500

    Fixed README generator and added output folder

---
+++ README.md
@@ -0,0 +1,35 @@
+# ProfessionalREADMEGenerator
+  
+![License Badge](https://img.shields.io/badge/license-MIT-blue)
+
+## Description
+A command-line application that generates a professional README file based on user input
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Clone the repository, run npm install to install the necessary dependencies, and then run node index.js to start the application.
+
+## Usage
+To use the application, simply run node index.js, answer the prompts, and the README file will be generated in the root directory.
+
+## License
+
+This project is licensed under the MIT license. See the [LICENSE](./LICENSE) file for details.
+
+## Contributing
+Feel free to fork the repository, create a new branch, and submit a pull request. Please follow the code of conduct and contribute responsibly.
+
+## Tests
+After running the generator, check the newly created README.md file in the project root to ensure the data entered was correctly formatted.
+
+## Questions
+If you have any questions, feel free to reach me at:
+- Email: [calebparker256@gmail.com](mailto:calebparker256@gmail.com)
+- GitHub: [CJdacoda](https://github.com/CJdacoda)
--- index.js
+++ index.js
@@ -1,13 +1,80 @@
-// TODO: Include packages needed for this application
-
+import inquirer from 'inquirer';
+import fs from 'fs';
+import path from 'path';
+import generateMarkdown from './utils/generateMarkdown.js';
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+    validate: (input) => (input ? true : 'Title is required!'),
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description of your project:',
+    validate: (input) => (input ? true : 'Description is required!'),
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?',
+    validate: (input) => (input ? true : 'Installation instructions are required!'),
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What is the usage information?',
+    validate: (input) => (input ? true : 'Usage information is required!'),
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What are the contribution guidelines?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What are the test instructions?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+    validate: (input) => (input ? true : 'GitHub username is required!'),
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+    validate: (input) => (input ? true : 'Email is required!'),
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const outputPath = path.join(process.cwd(), 'output');
+  if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath);
+  }
+  fs.writeFileSync(path.join(outputPath, fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdownContent = generateMarkdown(answers); // Generate the README content
+    writeToFile('README.md', markdownContent); // Write the content to a file
+    console.log('README.md generated successfully in the output folder!');
+  });
+}
 
 // Function call to initialize app
 init();
